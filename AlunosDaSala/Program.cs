6e41fb0a08594feca2317f2a945f0836b37ee168
Diego Pollheim <62944421+diegopollheim@users.noreply.github.com>
2020-09-14 19:24:43 -0300

    Adicionando Arquivos

---
+++ AlunosDaSala/Program.cs
@@ -0,0 +1,36 @@
+﻿1
+using System;
+
+namespace AlunosDaSala
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Quantos alunos tem a turma?");
+            int qtdAlunos = int.Parse(Console.ReadLine());
+            Console.Clear();
+
+            string[] nome = new string[qtdAlunos];
+            double[] nota = new double[qtdAlunos];
+            double melhorNota = 0;
+            string melhorAluno = "";
+            
+            for (int i = 0; i < nome.Length; i++)
+            {
+                Console.WriteLine("Nome do {0}º Aluno: ", i + 1);
+                nome[i] = Console.ReadLine();
+                Console.WriteLine("Nota Aluno: ");
+                nota[i] = double.Parse(Console.ReadLine());
+                if (nota[i] > melhorNota)
+                {
+                    melhorNota = nota[i]; 
+                    melhorAluno = nome[i];
+                }
+            }
+            Console.WriteLine("O aluno com melhor aproveitamento foi {0} com a nota {1}", melhorAluno, melhorNota);
+            Console.ReadKey();
+        
+        }
+    }
+}
---
+++ Arrays/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] nomes = new string[10];
+            double[] notas = new double[10];
+            double somaNota = 0; // Pega as notas para fazer a média no final.
+
+
+            for (int cont = 0; cont < nomes.Length; cont++) // Pelo meu conhecimento dentro de um for a variável criada é apenas local.
+            {
+                Console.WriteLine("{0}º Aluno: ", cont + 1);
+                nomes[cont] = Console.ReadLine();
+                Console.WriteLine("Nota: ");
+                notas[cont] = double.Parse(Console.ReadLine());
+                somaNota += notas[cont]; // Cada vez que for digitada uma nota ela sera colocada dentro da variável somaNota.
+                Console.Clear();
+            }
+
+            Console.WriteLine(" Aluno |  Nota");
+            Console.WriteLine();
+
+            Array.Sort(nomes, notas); // tirar a duvida do porque ele deu as notas de cada aluno corretamente
+
+            for (int cont = 0; cont < nomes.Length; cont++)
+            {
+
+                Console.WriteLine(" {0} - {1} ", nomes[cont], notas[cont].ToString("N1"));
+
+            }
+
+            double media = somaNota / notas.Length;  // Aqui pego todas as notas somadas dentro da varivel somaNota e divido pelo tamanho da Array notas.
+            Console.WriteLine();
+            Console.WriteLine(" A média da sala foi: {0}", media.ToString("N1")); // Escreve na tela a média com formatação de 1 numero apos a vírgula.
+            Console.ReadKey();
+        }
+    }
+}
