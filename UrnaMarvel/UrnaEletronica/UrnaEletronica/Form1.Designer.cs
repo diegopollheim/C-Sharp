03795a1e3b80aa15ce4b5a999392f9c7278da71a
TARCISIO NUNES <diegopollheim@gmail.com>
2021-09-01 21:13:36 -0300

    urna adicionada

---
+++ UrnaMarvel/UrnaEletronica/UrnaEletronica/Form1.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace UrnaEletronica
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.b = new System.Windows.Forms.Button();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.Label();
+            this.txtNumero = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.btnConfirma = new System.Windows.Forms.Button();
+            this.btnCorrige = new System.Windows.Forms.Button();
+            this.btnBranco = new System.Windows.Forms.Button();
+            this.telaFoto = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.bntH = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.telaFoto)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // b
+            // 
+            this.b.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.b.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.b.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.b.Location = new System.Drawing.Point(375, 17);
+            this.b.Name = "b";
+            this.b.Size = new System.Drawing.Size(112, 57);
+            this.b.TabIndex = 1;
+            this.b.Text = "1";
+            this.b.UseVisualStyleBackColor = false;
+            this.b.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNome.Location = new System.Drawing.Point(14, 359);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(83, 25);
+            this.lblNome.TabIndex = 2;
+            this.lblNome.Text = "NOME:";
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(14, 324);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(112, 25);
+            this.lblNumero.TabIndex = 3;
+            this.lblNumero.Text = "NÚMERO:";
+            // 
+            // txtNome
+            // 
+            this.txtNome.AutoSize = true;
+            this.txtNome.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNome.Location = new System.Drawing.Point(106, 356);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(0, 29);
+            this.txtNome.TabIndex = 4;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.AutoSize = true;
+            this.txtNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero.Location = new System.Drawing.Point(123, 321);
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(0, 29);
+            this.txtNumero.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.Location = new System.Drawing.Point(505, 17);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 57);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "2";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.Location = new System.Drawing.Point(635, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(112, 57);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "3";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.Location = new System.Drawing.Point(635, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(112, 57);
+            this.button3.TabIndex = 10;
+            this.button3.Text = "6";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.Location = new System.Drawing.Point(505, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(112, 57);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "5";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.Location = new System.Drawing.Point(375, 94);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(112, 57);
+            this.button5.TabIndex = 8;
+            this.button5.Text = "4";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.Location = new System.Drawing.Point(635, 172);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(112, 57);
+            this.button6.TabIndex = 13;
+            this.button6.Text = "9";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.Location = new System.Drawing.Point(505, 172);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(112, 57);
+            this.button7.TabIndex = 12;
+            this.button7.Text = "8";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.Location = new System.Drawing.Point(375, 172);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(112, 57);
+            this.button8.TabIndex = 11;
+            this.button8.Text = "7";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.Location = new System.Drawing.Point(505, 248);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(112, 57);
+            this.button9.TabIndex = 14;
+            this.button9.Text = "0";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.CliqueBotao);
+            // 
+            // btnConfirma
+            // 
+            this.btnConfirma.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.btnConfirma.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnConfirma.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnConfirma.Location = new System.Drawing.Point(635, 321);
+            this.btnConfirma.Name = "btnConfirma";
+            this.btnConfirma.Size = new System.Drawing.Size(112, 74);
+            this.btnConfirma.TabIndex = 17;
+            this.btnConfirma.Text = "Confirma";
+            this.btnConfirma.UseVisualStyleBackColor = false;
+            this.btnConfirma.Click += new System.EventHandler(this.btnConfirma_Click);
+            // 
+            // btnCorrige
+            // 
+            this.btnCorrige.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(64)))), ((int)(((byte)(0)))));
+            this.btnCorrige.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCorrige.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnCorrige.Location = new System.Drawing.Point(505, 324);
+            this.btnCorrige.Name = "btnCorrige";
+            this.btnCorrige.Size = new System.Drawing.Size(112, 74);
+            this.btnCorrige.TabIndex = 16;
+            this.btnCorrige.Text = "Corrige";
+            this.btnCorrige.UseVisualStyleBackColor = false;
+            this.btnCorrige.Click += new System.EventHandler(this.btnCorrige_Click);
+            // 
+            // btnBranco
+            // 
+            this.btnBranco.BackColor = System.Drawing.SystemColors.Window;
+            this.btnBranco.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBranco.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.btnBranco.Location = new System.Drawing.Point(375, 324);
+            this.btnBranco.Name = "btnBranco";
+            this.btnBranco.Size = new System.Drawing.Size(112, 74);
+            this.btnBranco.TabIndex = 15;
+            this.btnBranco.Text = "Branco";
+            this.btnBranco.UseVisualStyleBackColor = false;
+            this.btnBranco.Click += new System.EventHandler(this.btnBranco_Click);
+            // 
+            // telaFoto
+            // 
+            this.telaFoto.Location = new System.Drawing.Point(19, 12);
+            this.telaFoto.Name = "telaFoto";
+            this.telaFoto.Size = new System.Drawing.Size(326, 306);
+            this.telaFoto.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.telaFoto.TabIndex = 0;
+            this.telaFoto.TabStop = false;
+            // 
+            // bntH
+            // 
+            this.bntH.BackColor = System.Drawing.Color.Blue;
+            this.bntH.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bntH.ForeColor = System.Drawing.Color.White;
+            this.bntH.Location = new System.Drawing.Point(307, 342);
+            this.bntH.Name = "bntH";
+            this.bntH.Size = new System.Drawing.Size(38, 39);
+            this.bntH.TabIndex = 18;
+            this.bntH.Text = "H";
+            this.bntH.UseVisualStyleBackColor = false;
+            this.bntH.Click += new System.EventHandler(this.bntH_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(763, 403);
+            this.Controls.Add(this.bntH);
+            this.Controls.Add(this.btnConfirma);
+            this.Controls.Add(this.btnCorrige);
+            this.Controls.Add(this.btnBranco);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtNumero);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.lblNome);
+            this.Controls.Add(this.b);
+            this.Controls.Add(this.telaFoto);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.telaFoto)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button b;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label txtNome;
+        private System.Windows.Forms.Label txtNumero;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button btnConfirma;
+        private System.Windows.Forms.Button btnCorrige;
+        private System.Windows.Forms.Button btnBranco;
+        private System.Windows.Forms.PictureBox telaFoto;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button bntH;
+    }
+}
+
---
+++ UrnaMarvel/UrnaEletronica/UrnaEletronica/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+
+namespace UrnaEletronica
+{
+    public partial class Form1 : Form
+    {
+        string[] imagens = Directory.GetFiles(@"C:\Users\Aluno\Downloads\UrnaEletronica\vingadores");
+        string[] candidato = new string[5] { "Hulk", "Homem de Ferro", "Thor", "Viúva Negra", "Capitão América" };
+        string[] numero = new string[5] { "25", "35", "45", "55", "65" };
+        string aux;
+
+        //int cont1, cont2, cont3, cont4, cont5;
+        int[] cont = new int[5];
+
+        Timer tempo = new Timer();
+
+        public Form1()
+        {
+            InitializeComponent();
+            telaFoto.Image = Image.FromFile(imagens[imagens.Length - 2]);
+        }
+
+        private void CliqueBotao(object sender, EventArgs e)
+        {
+            Button btn = sender as Button;
+
+            aux += btn.Text;
+
+            if (aux.Length == 2)
+            {
+                for (int i = 0; i < numero.Length; i++)
+                {
+                    if (aux == numero[i])
+                    {
+                        telaFoto.Image = Image.FromFile(imagens[i]);
+                        txtNumero.Text = numero[i];
+                        txtNome.Text = candidato[i];
+                    }
+                }
+                aux = null;
+            }
+
+        }
+
+        private void btnBranco_Click(object sender, EventArgs e)
+        {
+            txtNumero.Text = "";
+            txtNome.Text = "";
+            telaFoto.Image = Image.FromFile(imagens[imagens.Length - 5]);
+        }
+
+        private void btnCorrige_Click(object sender, EventArgs e)
+        {
+            txtNumero.Text = "";
+            txtNome.Text = "";
+            telaFoto.Image = Image.FromFile(imagens[imagens.Length - 2]);
+        }
+
+        private void btnConfirma_Click(object sender, EventArgs e)
+        {
+            if (txtNumero.Text == numero[0])
+            {
+                cont[0]++;
+            }
+
+            if (txtNumero.Text == numero[1])
+            {
+                cont[1]++;
+            }
+
+            if (txtNumero.Text == numero[2])
+            {
+                cont[2]++;
+            }
+
+            if (txtNumero.Text == numero[3])
+            {
+                cont[3]++;
+            }
+
+            if (txtNumero.Text == numero[4])
+            {
+                cont[4]++;
+            }
+
+            if (txtNumero.Text != "")
+            {
+                txtNumero.Text = "";
+                txtNome.Text = "";
+                telaFoto.Image = Image.FromFile(imagens[imagens.Length - 3]);
+
+                tempo.Start();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            tempo.Interval = 2000;
+            tempo.Tick += Tempo_Tick;
+        }
+
+        private void Tempo_Tick(object sender, EventArgs e)
+        {
+            tempo.Stop();
+            telaFoto.Image = Image.FromFile(imagens[imagens.Length - 2]);
+        }
+
+        private void bntH_Click(object sender, EventArgs e)
+        {
+
+            int qtdVotos = 0;
+            string nomeVencedor = "";
+
+            for (int i = 0; i < candidato.Length; i++)
+            {
+                if (cont[i] > qtdVotos)
+                {
+                    qtdVotos = cont[i];
+                    nomeVencedor = candidato[i];
+                }
+            }
+
+            MessageBox.Show($"{candidato[0]} - {cont[0]}" + "\n" +
+                            $"{candidato[1]} - {cont[1]}" + "\n" +
+                            $"{candidato[2]} - {cont[2]}" + "\n" +
+                            $"{candidato[3]} - {cont[3]}" + "\n" +
+                            $"{candidato[4]} - {cont[4]}" + "\n\n" +
+                            $"{nomeVencedor} está vencendo com {qtdVotos} votos !","Histórico Votação");
+        }
+    }
+}
